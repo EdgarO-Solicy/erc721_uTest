353fe07d63336899a3eb1285a77b4d16c8ae9f07
Edgar Ohanyan <edgarohanyan1994@gmail.com>
2022-11-22 18:22:02 +0400

    changed memory managment

--- erc721-tests/contracts/Erc-721.sol
+++ erc721-tests/contracts/Erc-721.sol
@@ -11,8 +11,7 @@ contract Erc721 is Ownable, ERC721("SolicyNFT", "HNFT") {
     string public _name;
     string public _symbol;
     string public __baseURI;
-    string public uriSuffix = ".json";
-    mapping(address => TokenMetaData[]) public ownershipRecord;
+    mapping(uint256 => TokenMetaData) public metadataRecord;
     mapping(uint256 => bool) internal lockRecord;
 
     struct TokenMetaData {
@@ -44,24 +43,26 @@ contract Erc721 is Ownable, ERC721("SolicyNFT", "HNFT") {
         return _symbol;
     }
 
-    function _baseURI() internal view virtual override returns (string memory) {
+    function baseURI() public view virtual returns (string memory) {
         return __baseURI;
     }
 
     function tokenURI(uint256 tokenId_) public view virtual override returns (string memory) {
         _requireMinted(tokenId_);
 
-        return bytes(_baseURI()).length > 0 ? string(abi.encodePacked(_baseURI(), Strings.toString(tokenId_), uriSuffix)) : "";
+        return bytes(baseURI()).length > 0 ? string(abi.encodePacked(baseURI(), Strings.toString(tokenId_), ".json")) : "";
     }
 
     function mintToken(address recipient, string memory name_) onlyOwner public {
         require(owner()!=recipient, "Recipient cannot be the owner of the contract");
         _safeMint(recipient, tokenId);
-        ownershipRecord[recipient].push(TokenMetaData(name_, tokenId, 0, 0, tokenURI(tokenId), 0, 0));
+
+        metadataRecord[tokenId] = TokenMetaData(name_, tokenId, 0, 0, tokenURI(tokenId), 0, 0);
+
         tokenId = tokenId + 1;
     }
 
-    function transfer(
+    function transfer (
         address from_,
         address to_,
         uint256 tokenId_
@@ -73,75 +74,56 @@ contract Erc721 is Ownable, ERC721("SolicyNFT", "HNFT") {
         require(_ownerOf(tokenIdToBurn) != address(0), "ERC721: invalid token ID");
         require(tokenIdToTransferExp > 0, "ERC721: invalid reciver token ID");
 
-        uint senderexp = (getTockenMetadata(tokenIdToBurn).experience / 100) * 80;
-        getTockenMetadata(uint(tokenIdToTransferExp)).experience += senderexp;
+        uint senderexp = (metadataRecord[tokenIdToBurn].experience * 80) / 100;
+        metadataRecord[uint256(tokenIdToTransferExp)].experience += senderexp;
 
         _burn(tokenIdToBurn);
     }
 
-    function getTockenMetadata(uint256 tokenId_) public view returns(TokenMetaData memory) {
-        address tokenAddress = _ownerOf(tokenId_);
-        TokenMetaData[] memory dataList = ownershipRecord[tokenAddress];
-        for (uint256 i = 0; i < dataList.length; i++) {
-            if (dataList[i].tokenId == tokenId_) {
-                return dataList[i];
-            }
-        }
-    }
 
     function lockToken(uint256 tokenId_, uint256 daysToLock) public isNotLocked(tokenId_) {
-        TokenMetaData memory tokenMetaData_ = getTockenMetadata(tokenId_);
+        metadataRecord[tokenId_].lockedTimeStamp = block.timestamp;
+        metadataRecord[tokenId_].daysToLock = daysToLock;
+
 
         lockRecord[tokenId_] = true;
-        tokenMetaData_ = TokenMetaData(
-            tokenMetaData_.name, tokenId_, block.timestamp, daysToLock, 
-            tokenURI(tokenId_), tokenMetaData_.experience, tokenMetaData_.rank
-        );
     }
 
     function unLockToken(uint256 tokenId_) public {
-        TokenMetaData memory tokenMetaData_ = getTockenMetadata(tokenId_);
-        uint256 lockedTimeStamp_ = tokenMetaData_.lockedTimeStamp;
-        uint256 daysToLock_ = tokenMetaData_.daysToLock;
+        uint256 lockedTimeStamp_ =  metadataRecord[tokenId_].lockedTimeStamp;
+        uint256 daysToLock_ = metadataRecord[tokenId_].daysToLock;
 
-        uint diff = (block.timestamp - lockedTimeStamp_) / 60 / 60 / 24; 
+        uint diff = (block.timestamp - lockedTimeStamp_) / 60 / 60 / 24;
         bool overLockDeadline = diff >= daysToLock_;
 
         require(overLockDeadline, "The token lock time is pending ...");
 
         addExp(tokenId_, diff * 100);
+
+        metadataRecord[tokenId_].lockedTimeStamp = 0;
+        metadataRecord[tokenId_].daysToLock = 0;
         lockRecord[tokenId_] = false;
-        tokenMetaData_ = TokenMetaData(
-            tokenMetaData_.name, tokenId_, tokenMetaData_.lockedTimeStamp, 0, 
-            tokenURI(tokenId_), tokenMetaData_.experience, tokenMetaData_.rank
-        );
     }
 
     function claimExp(uint256 tokenId_) public {
-        TokenMetaData memory tokenMetaData_ = getTockenMetadata(tokenId_);
-        uint diff = (block.timestamp - tokenMetaData_.lockedTimeStamp) / 60 / 60 / 24; 
-
+        uint diff = (block.timestamp -  metadataRecord[tokenId_].lockedTimeStamp) / 60 / 60 / 24; 
         addExp(tokenId_, diff * 100);
-        tokenMetaData_ = TokenMetaData(
-            tokenMetaData_.name, tokenId_, block.timestamp, tokenMetaData_.daysToLock - diff, 
-            tokenURI(tokenId_), tokenMetaData_.experience, tokenMetaData_.rank
-        );
+        
+        metadataRecord[tokenId_].lockedTimeStamp = block.timestamp;
+        metadataRecord[tokenId_].daysToLock -= diff;
+
     }
 
-    function addExp(uint256 tokenId_, uint256 expToAdd) internal {
-        TokenMetaData memory tokenMetaData_ = getTockenMetadata(tokenId);
-
-        tokenMetaData_ = TokenMetaData(
-            tokenMetaData_.name, tokenId_, tokenMetaData_.lockedTimeStamp, tokenMetaData_.daysToLock, 
-            tokenURI(tokenId_), tokenMetaData_.experience + expToAdd, tokenMetaData_.rank
-        );
+    function addExp(uint256 tokenId_, uint256 expToAdd) public {
+        metadataRecord[tokenId_].experience += expToAdd;
     }
 
     function runkUp(uint tokenId_) external {
-        TokenMetaData memory tokenMetaData_ = getTockenMetadata(tokenId);
-        uint experience_ = tokenMetaData_.experience;
+        uint experience_ = metadataRecord[tokenId_].experience;
         uint rank_ = (experience_ - (experience_ % 1000)) / 1000;
 
-        tokenMetaData_ = TokenMetaData(tokenMetaData_.name, tokenId_, 0, 0, tokenURI(tokenId), experience_, rank_);
+        metadataRecord[tokenId_].experience = experience_;
+        metadataRecord[tokenId_].rank = rank_;
     }
 }
+
